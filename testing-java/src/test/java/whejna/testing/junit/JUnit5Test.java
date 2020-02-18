541495d3d33a155f02ef0bc8aadae316c442678c
Wojtek Hejna <whejna@google.com>
2020-02-18 16:50:20 +0000

    cleanup

--- testing-java/src/test/java/whejna/testing/junit/JUnit5Test.java
+++ testing-java/src/test/java/whejna/testing/junit/JUnit5Test.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import whejna.testing.HelloWorld;
-
 public class JUnit5Test {
     @Test
     public void justASimpleTest() {
