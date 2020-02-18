3796f3a1482031494e4adaa868677a92af67e0c6
Wojtek Hejna <whejna@google.com>
2020-02-18 17:02:16 +0000

    dsdfsa

---
+++ testing-java/src/main/java/Point.java
@@ -0,0 +1,13 @@
+package whejna.testing;
+public class Point {
+
+    public Point(int x, int y){
+
+    }
+    public int getX() {
+        return 0;
+    }
+    public int getY() {
+        return 0;
+    }
+}
--- testing-java/src/test/java/whejna/testing/junit/JUnit5Test.java
+++ testing-java/src/test/java/whejna/testing/junit/JUnit5Test.java
@@ -3,16 +3,22 @@ package whejna.testing.junit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import whejna.testing.Point;
+
 
 public class JUnit5Test {
     @Test
     public void justASimpleTest() {
+        Point p = new Point(0, 0);
+
+        assertEquals(p.getX(), 0);
+        assertEquals(p.getY(), 0);
         assertEquals(0, 0, "it should be 0");
     }
 
-    @Test
-    public void testAnotherHelloWorldClass() {
-        HelloWorld helloWorld = new HelloWorld();
-        assertEquals("JAMES", helloWorld.sayHelloUpper("James"));
-    }
+//    @Test
+//    public void testAnotherHelloWorldClass() {
+//        HelloWorld helloWorld = new HelloWorld();
+//        assertEquals("JAMES", helloWorld.sayHelloUpper("James"));
+//    }
 }
